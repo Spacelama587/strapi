5641b2f74aa70853805eae450c1d75dc706b2cc4
thevijeeth@gmail.com <thevijeeth@gmail.com>
2024-04-17 08:08:35 +0530

    for dep

---
+++ src/api/news-letter-signup/routes/news-letter-signup.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * news-letter-signup router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::news-letter-signup.news-letter-signup');
---
+++ src/api/news-letter-signup/services/news-letter-signup.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * news-letter-signup service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::news-letter-signup.news-letter-signup');
